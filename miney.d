5d29138b120efa15c52ec3f08d0b297267f2636c
Ligustah <andre@bierlein-berlin.de>
2011-03-04 13:04:44 +0100

    ---

--- miney.d
+++ miney.d
@@ -3,12 +3,32 @@ module miney;
 import minid.api;
 import minid.bind;
 
+import tango.net.device.Socket;
+
+import tango.io.selector.Selector;
+import tango.io.stream.Buffered;
+import tango.io.Stdout;
+
+import tango.core.Thread;
+
+import protocol;
+
 void main()
 {
+	//Stdout(Receivable);
+	
 	MDVM vm;
 	
 	auto t = openVM(&vm);
 	loadStdlibs(t, MDStdlib.All);
 	
 	runFile(t, "miney.md");
+	
+	return;
+	
+	auto s = new Selector();
+	s.open;
+	
+	Socket sock = new Socket();
+	sock.connect("Andre-PC", 25565);
 }
--- protocol.d
+++ protocol.d
@@ -1,8 +1,38 @@
 module protocol;
 
+import tango.io.stream.Data;
+import tango.io.device.Conduit;
+
+import tango.io.Stdout;
+
 static const int ProtocolVersion = 9;
 
-enum : byte
+static ClassInfo[PacketID] PacketHandlers;
+
+static bool implements(ClassInfo ci, ClassInfo interfac)
+{
+	foreach(i; ci.interfaces)
+	{
+		if(i.classinfo is interfac)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+static void addHandler(ClassInfo ci)
+{
+	scope Receivable o = cast(Receivable)ci.create();
+	
+	if(o)
+	{
+		PacketHandlers[o.packetID] = ci;
+		Stdout.formatln("adding handler for packet 0x{:x} \"{}\"", o.packetID, ci.name);
+	}
+}
+
+enum PacketID : ubyte
 {
 	KeepAlive = 0x00,
 	Login = 0x01,
@@ -58,12 +88,371 @@ enum : byte
 	Disconnect = 0xFF
 }
 
-class Packet
+public bool variableLength(PacketID type)
 {
-	public abstract byte id;
-	public abstract bool valid;
+	switch(type)
+	{
+		case PacketID.Login:
+		case PacketID.Handshake:
+		case PacketID.Chat:
+		case PacketID.PlayerBlockPlacement:
+		case PacketID.NamedEntitySpawn:
+		case PacketID.MobSpawn:
+		case PacketID.EntityPainting:
+		case PacketID.EntityMetadata:
+		case PacketID.MapChunk:
+		case PacketID.MultiBlockChange:
+		case PacketID.Explosion:
+		case PacketID.WindowClick:
+		case PacketID.SetSlot:
+		case PacketID.WindowItems:
+		case PacketID.UpdateSign:
+		case PacketID.Disconnect:
+			return true;
+	}
+	
+	return false;
 }
 
-class LoginRequest
+class MinecraftDataOutput : DataOutput
 {
+	this(OutputStream outs)
+	{
+		super(outs);
+		endian(Network);
+	}
+	
+	public alias string putString;
+	
+	public size_t string(char[] str)
+	{
+		int16(str.length);
+		return write(str);
+	}
+}
+
+class MinecraftDataInput : DataInput
+{
+	this(InputStream ins)
+	{
+		super(ins);
+		endian(Network);
+	}
+	
+	public alias string getString;
+	
+	public char[] string()
+	{
+		short len = int16;
+		Stdout.formatln("attempting to read {} chars", len);
+		char[] str = new char[len];
+		read(cast(void[])str);
+		
+		return str;
+	}
+}
+
+interface Sendable
+{
+	public void send(MinecraftDataOutput);
+	public PacketID packetID();
+}
+
+interface Receivable
+{
+	public PacketID packetID();
+	public size_t minSize();
+	public int receive(MinecraftDataInput);
+}
+
+class KeepAlive : Receivable, Sendable
+{
+	static this()
+	{
+		addHandler(KeepAlive.classinfo);
+	}
+	
+	public size_t minSize()
+	{
+		return 1;
+	}
+	
+	public PacketID packetID()
+	{
+		return PacketID.KeepAlive;
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		return minSize;
+	}
+	
+	public void send(MinecraftDataOutput output)
+	{
+		output.putByte(packetID);
+	}
+	
+	public this()
+	{
+	}
+}
+
+class Login : Sendable, Receivable
+{
+	static this()
+	{
+		addHandler(Login.classinfo);
+	}
+	
+	union
+	{
+		private int _protocolVersion;
+		private int _entityID;
+	}
+	private char[] _username;
+	private char[] _password;
+	private long _mapSeed;
+	private byte _dimension;
+	
+	public size_t minSize()
+	{
+		return 18;
+	}
+	
+	public PacketID packetID()
+	{
+		return PacketID.Login;
+	}
+	
+	public this(char[] username, char[] password)
+	{
+		this._protocolVersion = ProtocolVersion;
+		this._username = username;
+		this._password = password;
+	}
+	
+	public this()
+	{
+		
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		_entityID = input.getInt();
+		_username = input.getString();
+		_password = input.getString();
+		_mapSeed = input.getLong();
+		_dimension = input.getByte();
+		
+		return minSize + _username.length + _password.length;
+	}
+	
+	public void send(MinecraftDataOutput output)
+	{
+		output.putByte(packetID);
+		output.putInt(_protocolVersion);
+		output.putString(_username);
+		output.putString(_password);
+		output.putLong(0);	// map seed
+		output.putByte(0);	// dimension
+	}
+	
+	public int entityID()
+	{
+		return _entityID;
+	}
+	
+	public long mapSeed()
+	{
+		return _mapSeed;
+	}
+}
+
+class Handshake : Sendable, Receivable
+{
+	static this()
+	{
+		addHandler(Handshake.classinfo);
+	}
+	
+	union
+	{
+		private char[] _username;
+		private char[] _connectionHash;
+	}
+	
+	public static const char[] AUTH_NONE = "-";
+	public static const char[] AUTH_PASSWORD = "+";
+	
+	public size_t minSize()
+	{
+		return 3;
+	}
+	
+	public PacketID packetID()
+	{
+		return PacketID.Handshake;
+	}
+	
+	public this(char[] username)
+	{
+		this._username = username;
+	}
+	
+	public this()
+	{
+	}
+	
+	public char[] connectionHash()
+	{
+		return _connectionHash;
+	}
+	
+	public void send(MinecraftDataOutput output)
+	{
+		output.putByte(PacketID.Handshake);
+		output.putString(_username);
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		_connectionHash = input.getString();
+		
+		return minSize + _connectionHash.length;
+	}
+}
+
+class Chat : Sendable, Receivable
+{
+	static this()
+	{
+		addHandler(Chat.classinfo);
+	}
+	
+	char[] _message;
+	
+	public size_t minSize()
+	{
+		return 3;
+	}
+	
+	public PacketID packetID()
+	{
+		return PacketID.Chat;
+	}
+	
+	public this(char[] message)
+	{
+		this._message = message;
+	}
+	
+	public this()
+	{
+		
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		_message = input.getString();
+		
+		return minSize + _message.length;
+	}
+	
+	public void send(MinecraftDataOutput output)
+	{
+		output.putByte(packetID);
+		output.putString(_message);
+	}
+	
+	public char[] message()
+	{
+		return _message;
+	}
+	
+	public void message(char[] msg)
+	{
+		_message = msg;
+	}
+}
+
+class TimeUpdate : Receivable
+{
+	static this()
+	{
+		addHandler(TimeUpdate.classinfo);
+	}
+	
+	private long _time;
+	
+	public PacketID packetID()
+	{
+		return PacketID.TimeUpdate;
+	}
+	
+	public size_t minSize()
+	{
+		return 9;
+	}
+	
+	public this()
+	{
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		_time = input.getLong();
+		
+		return minSize;
+	}
+	
+	public long time()
+	{
+		return _time;
+	}
+}
+
+class SpawnPosition : Receivable
+{
+	static this()
+	{
+		addHandler(SpawnPosition.classinfo);
+	}
+	
+	private int _x, _y, _z;
+	
+	public size_t minSize()
+	{
+		return 13;
+	}
+	
+	public PacketID packetID()
+	{
+		return PacketID.SpawnPosition;
+	}
+	
+	public int receive(MinecraftDataInput input)
+	{
+		_x = input.getInt();
+		_y = input.getInt();
+		_z = input.getInt();
+		
+		return minSize;
+	}
+}
+
+public class Player : Sendable
+{
+	static this()
+	{
+		addHandler(Player.classinfo);
+	}
+	
+	private bool _onGround;
+	
+	public this(bool onGround)
+	{
+		_onGround = onGround;
+	}
+	
+	
 }
